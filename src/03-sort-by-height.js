ac0388f04ee29914ffd401e9c889f7ebe7b9e2a1
Vladislav Tur <vla-tur@yandex.ru>
2021-04-13 00:31:19 +0400

    feat: solve 3

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,15 +9,27 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sortArr = arr.filter((e) => e !== -1).sort((a, b) => a - b);
+  const indexArr = [];
+  const resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      indexArr.push(i);
+    }
+  }
+
+  arr.map((e, index) => {
+    if (indexArr.indexOf(index) !== -1) {
+      resultArr.push(-1);
+    } else {
+      resultArr.push(sortArr[0]);
+      sortArr.shift();
+    } return resultArr;
+  });
+
+  return resultArr;
 }
 
 module.exports = sortByHeight;
-
-/*
-  1. Make 2 empty arrrays arr1 and arr2
-  2.
-сначала нашел индексы элементов которые -1,
-потом отсортировал без них далее по индексам в
-массив после сортировки -1 вставлял */
