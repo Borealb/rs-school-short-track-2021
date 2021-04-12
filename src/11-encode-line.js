9d3c07495b4a2e9c2ff28c5def2a7bd34dc49be8
Vladislav Tur <vla-tur@yandex.ru>
2021-04-13 02:00:01 +0400

    feat: solve 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let number = 1;
+  let resultStr = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      number++;
+    } else if (number !== 1) {
+      resultStr += number + arr[i];
+      number = 1;
+    } else {
+      resultStr += arr[i];
+    }
+  }
+
+  return resultStr;
 }
 
 module.exports = encodeLine;
